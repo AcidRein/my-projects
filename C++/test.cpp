3342a83249e88928c402ce719501549eed29f91f
Seng <sengforschool@example.com>
2025-05-15 17:28:25 +0930

    15/05/2025

---
+++ C++/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <set>
+using namespace std;
+
+int main(void){
+    string myStr = "Racts";
+    cout << myStr << endl;
+
+    reverse(myStr.begin(), myStr.end());
+    cout << myStr << endl;
+    
+}
