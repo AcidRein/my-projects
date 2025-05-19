679173cbf8ec94cf86ad1b458c4f466610ff149a
Seng <sengforschool@example.com>
2025-05-19 14:16:08 +0930

    19/05/2025

--- C++/AUCPL/stack_it.cpp
+++ C++/AUCPL/stack_it.cpp
@@ -1,17 +1,32 @@
 #include <bits/stdc++.h>
+#include <cmath>
 using namespace std;
 
+long double findRoot(int balls) {
+    int a = 1, b = 3, c = 2, d = -6 * balls;
+
+    double p = (3*a*c - b*b) / (3*a*a);
+    double q = (2*b*b*b - 9*a*b*c + 27*a*a*d) / (27*a*a*a);
+
+    // Discriminant
+    double discriminant = (q*q / 4) + (p*p*p / 27);
+
+    if (discriminant >= 0) {
+        double u = cbrt(-q/2 + std::sqrt(discriminant));
+        double v = cbrt(-q/2 - std::sqrt(discriminant));
+        return u + v - (b / (3 * a));
+
+    } else {
+        return NAN;
+    }
+}
+
 int main(void) {
-    long long numBalls; 
+    int numBalls; 
     
     cin >> numBalls;
-    long long height = (numBalls * (numBalls + 1) * (numBalls + 2)) / 6;
-
-    if ((numBalls * (numBalls + 1) * (numBalls + 2)) % 6 == 0) { // if int
-        cout << height << endl;
-    } else {
-        cout << height - 1 << endl;
-    }
+    long double res = findRoot(numBalls);
+    cout << floor(res) << endl;
 
     return 0;
 }
