1b58ea60fd00fb8f65e3075263042c85b61c82c7
Seng <sengforschool@example.com>
2025-05-20 13:15:57 +0930

    20/05/2025

--- C++/AUCPL/stack_it.cpp
+++ C++/AUCPL/stack_it.cpp
@@ -2,18 +2,18 @@
 #include <cmath>
 using namespace std;
 
-long double findRoot(int balls) {
-    int a = 1, b = 3, c = 2, d = -6 * balls;
+long double findRoot(long long balls) {
+    long long a = 1, b = 3, c = 2, d = -6 * balls;
 
-    double p = (3*a*c - b*b) / (3*a*a);
-    double q = (2*b*b*b - 9*a*b*c + 27*a*a*d) / (27*a*a*a);
+    long double p = (3*a*c - b*b) / (3*a*a);
+    long double q = (2*b*b*b - 9*a*b*c + 27*a*a*d) / (27*a*a*a);
 
     // Discriminant
-    double discriminant = (q*q / 4) + (p*p*p / 27);
+    long double discriminant = (q*q / 4) + (p*p*p / 27);
 
     if (discriminant >= 0) {
-        double u = cbrt(-q/2 + std::sqrt(discriminant));
-        double v = cbrt(-q/2 - std::sqrt(discriminant));
+        long double u = cbrt(-q/2 + std::sqrt(discriminant));
+        long double v = cbrt(-q/2 - std::sqrt(discriminant));
         return u + v - (b / (3 * a));
 
     } else {
@@ -22,11 +22,16 @@ long double findRoot(int balls) {
 }
 
 int main(void) {
-    int numBalls; 
+    long long numCustomers;
+    cin >> numCustomers;
+
+    for (long long i = 0; i < numCustomers; i++) {
+        long long numBalls; 
+        cin >> numBalls;
+        long double res = findRoot(numBalls);
+        cout << floor(lround(res* 1000000) / 1000000) << endl;
+    }
     
-    cin >> numBalls;
-    long double res = findRoot(numBalls);
-    cout << floor(res) << endl;
 
     return 0;
 }
